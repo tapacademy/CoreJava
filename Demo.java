9e8ddfd5a3eeeb699f0b91a1f2da992e18e53edb
TapAcad <124581045+TapAcad@users.noreply.github.com>
2023-02-07 16:12:55 +0530

    !InstanceVariable

---
+++ Demo.java
@@ -0,0 +1,15 @@
+!    class Demo{
+    int a;
+    float b;
+    boolean c;
+public static void main(String[] args)
+{
+Demo d = new Demo();
+d.a = 99;
+d.b = 99.9f;
+d.c = true;
+System.out.println(d.a);
+System.out.println(d.b);
+System.out.println(d.c);
+}
+}
