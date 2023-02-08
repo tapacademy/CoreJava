4c882ea2d1305d2394e0bbb7ea36fcd644deace3
TapAcad <124581045+TapAcad@users.noreply.github.com>
2023-02-08 16:37:29 +0530

    !jscon

---
+++ class Test{.java
@@ -0,0 +1,14 @@
+class Test{
+public static void main(String[] args)
+{
+    int a;
+    float b;
+    boolean c;
+a = 99;
+b = 99.9f;
+c = true;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}
